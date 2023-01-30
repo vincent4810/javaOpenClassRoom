8c4112668ad9e2a438af1940fda9ad05910453e6
vincent4810 <vincent.michel1989@gmail.com>
2023-01-30 12:37:39 +0100

    variables

---
+++ OpenClassRooms.java
@@ -0,0 +1,11 @@
+public class OpenClassRooms {
+    public static void main(String[] args){
+        int agePersonne = 19;
+
+        agePersonne = 45;
+
+        agePersonne = agePersonne + 28;
+
+        System.out.println("Age = " + agePersonne + " ans");
+    }
+}
