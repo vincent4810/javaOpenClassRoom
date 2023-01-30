0181378b12cc478a3a69000e627aeb67ba8fddf5
vincent4810 <vincent.michel1989@gmail.com>
2023-01-30 14:35:52 +0100

    cast type variables

---
+++ melangeTypeNum.java
@@ -0,0 +1,11 @@
+public class melangeTypeNum{
+    public static void main(String[] args){
+        int a = 5;
+
+        int b = 2;
+
+        float c = (float) a/b;
+        // cast (float)
+        System.out.println("Resultat : " + c);
+    }
+}
