07f4ec365437f812edc1a8ae0e41062140c78421
vincent4810 <vincent.michel1989@gmail.com>
2023-01-30 16:17:04 +0100

    methode sur class

---
+++ OC.java
@@ -0,0 +1,12 @@
+public class OC {
+    public static void main(String[] args){
+
+        String exemple = "hello";
+
+        exemple = exemple.toUpperCase();
+
+        exemple = exemple.replace("HELL", "YEAH");
+
+        System.out.println(exemple);
+    }
+}
