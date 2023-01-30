a9fb759b713a2bd722b27260adcd0840a5f4da98
vincent4810 <vincent.michel1989@gmail.com>
2023-01-30 16:38:43 +0100

    clean main function

---
+++ cleanHello/CleanWorld.java
@@ -0,0 +1,13 @@
+package cleanHello;
+
+
+public class CleanWorld {
+    public static void main(String[] args){
+        sayHelloTo("world");
+    }
+
+
+    private static void sayHelloTo(String recipient){
+        System.out.println("Hello " + recipient);
+    }
+}
