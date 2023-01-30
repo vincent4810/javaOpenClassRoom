f0632757297b803baf00df2e909e7ee00f902af9
vincent4810 <vincent.michel1989@gmail.com>
2023-01-30 13:59:51 +0100

    variable int commit

--- OpenClassRooms.java
+++ OpenClassRooms.java
@@ -1,11 +1,9 @@
-public class ManipulationVariables {
+public class OpenClassRooms{
     public static void main(String[] args){
         int agePersonne = 19;
 
-        agePersonne = 45;
-
-        agePersonne = agePersonne + 28;
-
+        agePersonne = 33;
+        agePersonne = agePersonne +28;
         System.out.println("Age = " + agePersonne + " ans");
     }
 }
