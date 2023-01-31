04fa68f58278f935b087ee67e48c24aabf164c42
vincent4810 <vincent.michel1989@gmail.com>
2023-01-31 22:00:32 +0100

    portée variables

---
+++ OCOne.java
@@ -0,0 +1,17 @@
+public class OCOne {
+    
+    static String exemple = "hello";
+
+    public static void main(String[] args){
+        fonction1();
+        fonction2();
+    }
+
+    public static void fonction1(){
+        System.out.println(exemple);
+    }
+
+    public static void fonction2(){
+        System.out.println(exemple);
+    }
+}
