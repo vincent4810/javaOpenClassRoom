f0f870e3246df6cbf6d8a8d64371955615a5bef8
vincent4810 <vincent.michel1989@gmail.com>
2023-01-31 23:33:11 +0100

    first foreach

--- OCBoucle.java
+++ OCBoucle.java
@@ -5,6 +5,14 @@ public class OCBoucle {
         for(int i=0; i<=1000; i++){
             bonjour(Integer.toString(i));
         }
+        int[] myArray = new int[]{7,2,4};
+        for(int i = 0; i < myArray.length; i++){
+            System.out.println(myArray[i]);
+        }
+
+        for(int numberToArray: myArray){
+            System.out.println(numberToArray);
+        }
     }
 
     static void bonjour(String nom){
